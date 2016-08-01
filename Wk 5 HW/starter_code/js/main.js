4ad58ef29885571df33f556444999facc7118387
ayanac <christieayana@gmail.com>
2016-07-31 21:31:15 -0400

    wk5_citypix_finalupdate

--- Wk 5 HW/starter_code/js/main.js
+++ Wk 5 HW/starter_code/js/main.js
@@ -8,29 +8,30 @@ submitButton.on("click", function(event){
 	//save user input 
 	var input = $("#city-type").val();
 	console.log(input)
+
 	//If a user submits: "New York" or "New York City" or "NYC" 
 	if (input=="NYC" || input=="New York City" || input=="New York"){
 	//make the background of the page nyc.jpg
-	$("body").addClass('nyc');
+	$("body").removeAttr('class').addClass('nyc');
 	}
 	// if user submits "San Francisco" or "SF" or "Bay Area"
 	else if (input==="San Francsico" || input==="SF" || input==="Bay Area"){
 	//make the background of the page sf.jpg
-	$("body").addClass('sf');
+	$("body").removeAttr('class').addClass('sf');
 	}
 	//If user submits "Los Angeles" or "LA" or "LAX"
 	else if (input==="Los Angeles" || input==="LA" || input==="LAX"){
 	////make the background of the page la.jpg
-	$("body").addClass('la');
+	$("body").removeAttr('class').addClass('la');
 	}
 	// If user submits "Austin" or "ATX"
 	else if (input==="Austin" || input==="ATX"){
 	//make the background of the page austin.jpg	
-	$("body").addCLass('austin');
+	$("body").removeAttr('class').addClass('austin');
 	}
 	// If user submits "Sydney" or "SYD" 
-	else (input==="sydney" || input==="SYD"){
-	$("body").addCLass('sydney');
+	else if (input==="Sydney" || input==="SYD"){
+	$("body").removeAttr('class').addClass('sydney');
 	}
 });
 });
